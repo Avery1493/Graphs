f3c369421301727b02b82c9687b95a265d841e34
Avery Quinn <avery.quinn1493@gmail.com>
2020-08-10 14:26:16 -0700

    init, add vertex, add edge, get neighbors

--- projects/graph/graph.py
+++ projects/graph/graph.py
@@ -7,25 +7,39 @@ class Graph:
 
     """Represent a graph as a dictionary of vertices mapping labels to edges."""
     def __init__(self):
+        # Adjacency List / empty dictionary
+        # Key: Node 
+        # Value: adjacent/connected node
         self.vertices = {}
 
     def add_vertex(self, vertex_id):
         """
         Add a vertex to the graph.
         """
-        pass  # TODO
+        # Add set of Nodes
+        self.vertices[vertex_id] = set()
 
     def add_edge(self, v1, v2):
         """
         Add a directed edge to the graph.
         """
-        pass  # TODO
+        # Add connection
+        # if Nodes exist
+        if v1 in self.vertices and v2 in self.vertices:
+            # connect them 
+            # ?? .add
+            self.vertices[v1].add(v2)
+        else:
+            raise IndexError("Vertex does not exits")
+
 
     def get_neighbors(self, vertex_id):
         """
         Get all neighbors (edges) of a vertex.
         """
-        pass  # TODO
+        # Return the set at vertexid
+        return self.vertices[vertex_id]
+
 
     def bft(self, starting_vertex):
         """
