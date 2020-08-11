e2a5731e65897fb6795c171565e5dee379129562
Avery Quinn <avery.quinn1493@gmail.com>
2020-08-11 13:37:50 -0700

    dfs recursive

--- projects/graph/graph.py
+++ projects/graph/graph.py
@@ -102,7 +102,7 @@ class Graph:
         """
         if visited is None:
             visited = set()
-        # add starting vertx
+        # add starting vertex
         visited.add(starting_vertex)
         # go through neighbors 
         for v in self.get_neighbors(starting_vertex):
@@ -157,7 +157,7 @@ class Graph:
         """
         # create empty stack
         s = Stack()
-        # enqueue path to starting vertex (list)
+        # push path to starting vertex (list)
         s.push([starting_vertex])
         # create set for visited
         visited = set()
@@ -177,14 +177,14 @@ class Graph:
                 # add path to neighbors to top of stack
                 for next_vertex in self.get_neighbors(path[-1]):
                     # make copy of path
-                    copy_path = path
+                    copy_path = list(path)
                     # append neighbor to path and push to path
                     copy_path.append(next_vertex)
                     s.push(copy_path)
         # return None
         return None 
 
-    def dfs_recursive(self, starting_vertex, destination_vertex):
+    def dfs_recursive(self, starting_vertex, destination_vertex, visited=None, path=None):
         """
         Return a list containing a path from
         starting_vertex to destination_vertex in
@@ -192,8 +192,26 @@ class Graph:
 
         This should be done using recursion.
         """
-        pass  # TODO
+        if visited is None:
+            visited = set()
+        if path is None:
+            path = []
+        # add starting vertex
+        visited.add(starting_vertex)
+        path = path + [starting_vertex]
+        # check if destination
+        if starting_vertex == destination_vertex:
+            return path
+        # go through neighbors 
+        for v in self.get_neighbors(starting_vertex):
+            # check if vertex has been visited
+            if v not in visited:
+                # run recursive call
+                new_path = self.dfs_recursive(v, destination_vertex, visited, path)
+                if new_path:
+                    return new_path
 
+        return None
 if __name__ == '__main__':
     graph = Graph()  # Instantiate your graph
     # https://github.com/LambdaSchool/Graphs/blob/master/objectives/breadth-first-search/img/bfs-visit-order.png
