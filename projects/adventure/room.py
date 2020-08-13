047ff440a2967166712edb5d43e51fb09c80484e
Avery Quinn <avery.quinn1493@gmail.com>
2020-08-13 15:39:16 -0700

    'direction'

--- projects/adventure/room.py
+++ projects/adventure/room.py
@@ -2,9 +2,9 @@
 # description attributes.
 class Room:
     def __init__(self, name, description, id=0, x=None, y=None):
-        self.id = id
-        self.name = name
-        self.description = description
+        self.id = id #2
+        self.name = name #Room2
+        self.description = description #(3,7)
         self.n_to = None
         self.s_to = None
         self.e_to = None
